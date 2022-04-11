92ddb94f687ecf796722b7d78ea4434f91b7af07
Dan <dannorthcott8@gmail.com>
2022-04-11 11:30:59 +0100

    all methods extracted - added tests all passing

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -60,7 +60,7 @@ describe('Gilded Rose', function () {
         ])
         const items = gildedRose.updateQuality()
 
-        expect(items[0].sellIn).toEqual(10)
+        expect(items[0].sellIn).toEqual(0)
         expect(items[0].quality).toEqual(80)
       })
     })
@@ -119,6 +119,105 @@ describe('Gilded Rose', function () {
   })
 })
 
+describe('Gilded Rose days', function () {
+  it('Day 1: should update all items', function () {
+    const startingItems = [
+      new Item('+5 Dexterity Vest', 10, 20),
+      new Item('Aged Brie', 2, 0),
+      new Item('Elixir of the Mongoose', 5, 7),
+      new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+      new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+
+      // This Conjured item does not work properly yet
+      new Item('Conjured Mana Cake', 3, 6)
+    ]
+
+    const gildedRose = new Shop(startingItems)
+    const items = gildedRose.updateQuality()
+
+    // +5 Dexterity Vest
+    expect(items[0].sellIn).toEqual(9)
+    expect(items[0].quality).toEqual(19)
+    // Aged Brie
+    expect(items[1].sellIn).toEqual(1)
+    expect(items[1].quality).toEqual(1)
+    // Elixir of the Mongoose
+    expect(items[2].sellIn).toEqual(4)
+    expect(items[2].quality).toEqual(6)
+    // Sulfuras, Hand of Ragnaros
+    expect(items[3].sellIn).toEqual(0)
+    expect(items[3].quality).toEqual(80)
+    // Sulfuras, Hand of Ragnaros
+    expect(items[4].sellIn).toEqual(0)
+    expect(items[4].quality).toEqual(80)
+    // Backstage passes to a TAFKAL80ETC concert
+    expect(items[5].sellIn).toEqual(14)
+    expect(items[5].quality).toEqual(21)
+    // Backstage passes to a TAFKAL80ETC concert
+    expect(items[6].sellIn).toEqual(9)
+    expect(items[6].quality).toEqual(50)
+    // Backstage passes to a TAFKAL80ETC concert
+    expect(items[7].sellIn).toEqual(4)
+    expect(items[7].quality).toEqual(50)
+    // Conjured Mana Cake
+    expect(items[8].sellIn).toEqual(2)
+    expect(items[8].quality).toEqual(4)
+  })
+
+  it('Day 30: should update all items', function () {
+    const startingItems = [
+      new Item('+5 Dexterity Vest', 10, 20),
+      new Item('Aged Brie', 2, 0),
+      new Item('Elixir of the Mongoose', 5, 7),
+      new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+      new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+
+      // This Conjured item does not work properly yet
+      new Item('Conjured Mana Cake', 3, 6)
+    ]
+
+    const gildedRose = new Shop(startingItems)
+    for (let i = 0; i < 29; i++) {
+      gildedRose.updateQuality()
+    }
+    const items = gildedRose.updateQuality()
+
+    // +5 Dexterity Vest
+    expect(items[0].sellIn).toEqual(-20)
+    expect(items[0].quality).toEqual(0)
+    // Aged Brie
+    expect(items[1].sellIn).toEqual(-28)
+    expect(items[1].quality).toEqual(50)
+    // Elixir of the Mongoose
+    expect(items[2].sellIn).toEqual(-25)
+    expect(items[2].quality).toEqual(0)
+    // Sulfuras, Hand of Ragnaros
+    expect(items[3].sellIn).toEqual(0)
+    expect(items[3].quality).toEqual(80)
+    // Sulfuras, Hand of Ragnaros
+    expect(items[4].sellIn).toEqual(0)
+    expect(items[4].quality).toEqual(80)
+    // Backstage passes to a TAFKAL80ETC concert
+    expect(items[5].sellIn).toEqual(-15)
+    expect(items[5].quality).toEqual(0)
+    // Backstage passes to a TAFKAL80ETC concert
+    expect(items[6].sellIn).toEqual(-20)
+    expect(items[6].quality).toEqual(0)
+    // Backstage passes to a TAFKAL80ETC concert
+    expect(items[7].sellIn).toEqual(-25)
+    expect(items[7].quality).toEqual(0)
+    // Conjured Mana Cake
+    expect(items[8].sellIn).toEqual(-27)
+    expect(items[8].quality).toEqual(0)
+  })
+})
+
 // 	- All items have a SellIn value which denotes the number of days we have to sell the item
 // 	- All items have a Quality value which denotes how valuable the item is
 // 	- At the end of each day our system lowers both values for every item
@@ -126,7 +225,7 @@ describe('Gilded Rose', function () {
 // 	- Once the sell by date has passed, Quality degrades twice as fast
 // 	- The Quality of an item is never negative
 // 	- "Aged Brie" actually increases in Quality the older it gets
-// 	- The Quality of an item is never more than 50
+// 	- The Quality of an item never increases over 50
 // 	- "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
 // 	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
 // 	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,69 +11,118 @@ class Shop {
     this.items = items
   }
 
+  updateAgedBrie(item) {
+    item.sellIn--
+
+    if (item.quality < 50) item.quality++
+
+    if (item.sellIn < 0 && item.quality < 50) item.quality++
+  }
+
+  updateBackstagePass(item) {
+    item.sellIn--
+
+    if (item.quality < 50) item.quality++
+
+    if (item.quality < 50 && item.sellIn < 11) item.quality++
+
+    if (item.quality < 50 && item.sellIn < 6) item.quality++
+
+    if (item.sellIn < 0) item.quality = 0
+  }
+
+  updateSulfuras(item) {
+    item.quality = 80
+    item.sellIn = 0
+  }
+
+  updateConjured(item) {
+    item.sellIn--
+
+    if (item.quality > 0) item.quality = item.quality - 2
+  }
+
+  updateNormalItem(item) {
+    item.sellIn--
+
+    if (item.quality > 0) item.quality--
+
+    if (item.quality > 0 && item.sellIn < 0) item.quality--
+  }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (
-        // if item name is not brie or backstage pass (increasers)
-        this.items[i].name != 'Aged Brie' &&
-        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-      ) {
-        // item quality greater than 0 and item name is not sulfuras (non changer) - decrease item quality by one
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
+      const item = this.items[i]
+
+      if (item.name === 'Aged Brie') {
+        this.updateAgedBrie(item)
+      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        this.updateBackstagePass(item)
+      } else if (item.name === 'Sulfuras, Hand of Ragnaros') {
+        this.updateSulfuras(item)
+      } else if (item.name.startsWith('Conjured')) {
+        this.updateConjured(item)
       } else {
-        // if item quality less than 50 increase item quality by one
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (
-            // if item name is backstage pass
-            this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            // if sellin in 10 or less ( removed: and quality is less than 50) increase item quality again
-            if (this.items[i].sellIn < 11) {
-              // if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1
-              // }
-            }
-            // if sellin is 5 or less increase item quality again
-            if (this.items[i].sellIn < 6) {
-              // if (this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1
-              // }
-            }
-          }
-        }
-      }
-      // if item name isnt sulfuras then decrement sellin
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1
-      }
-      // if item sellin less than 0 AND item isnt aged brie AND item name isnt backstage pass
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (
-            this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            // if item quality greater than 0 then decrement quality by one - as long as item quality isnt 0 decrement
-            if (this.items[i].quality > 0) {
-              // if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              this.items[i].quality = this.items[i].quality - 1
-              // }
-            }
-          } else {
-            // if sell in is less than 0 for backstage passes quality = 0
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          // if quality is less than 50 increment quality
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+        this.updateNormalItem(item)
+        // if (
+        //   // if item name is not brie or backstage pass (increasers)
+        //   item.name != 'Aged Brie' &&
+        //   item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        // ) {
+        //   // item quality greater than 0 and item name is not sulfuras (non changer) - decrease item quality by one
+        //   if (item.quality > 0) {
+        //     if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        //       item.quality = item.quality - 1
+        //     }
+        //   }
+        // } else {
+        //   // if item quality less than 50 increase item quality by one
+        //   if (item.quality < 50) {
+        //     item.quality = item.quality + 1
+        //     if (
+        //       // if item name is backstage pass
+        //       item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        //     ) {
+        //       // if sellin in 10 or less ( removed: and quality is less than 50) increase item quality again
+        //       if (item.sellIn < 11) {
+        //         if (item.quality < 50) {
+        //           item.quality = item.quality + 1
+        //         }
+        //       }
+        //       // if sellin is 5 or less increase item quality again
+        //       if (item.sellIn < 6) {
+        //         if (item.quality < 50) {
+        //           item.quality = item.quality + 1
+        //         }
+        //       }
+        //     }
+        //   }
+        // }
+        // // if item name isnt sulfuras then decrement sellin
+        // if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        //   item.sellIn = item.sellIn - 1
+        // }
+        // // if item sellin less than 0 AND item isnt aged brie AND item name isnt backstage pass
+        // if (item.sellIn < 0) {
+        //   if (item.name != 'Aged Brie') {
+        //     if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        //       // if item quality greater than 0 then decrement quality by one - as long as item quality isnt 0 decrement
+        //       if (item.quality > 0) {
+        //         // if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        //         item.quality = item.quality - 1
+        //         // }
+        //       }
+        //     } else {
+        //       // if sell in is less than 0 for backstage passes quality = 0
+        //       item.quality = item.quality - item.quality
+        //     }
+        //   } else {
+        //     // if quality is less than 50 increment quality
+        //     if (item.quality < 50) {
+        //       item.quality = item.quality + 1
+        //     }
+        //   }
+        // }
       }
     }
 
